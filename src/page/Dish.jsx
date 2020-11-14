15c56d502eb1e520286add6f76d17a4f7d2b1e8b
hiroki-harada <poketgame.0604@gmail.com>
2020-11-14 21:12:06 +0900

    get picture from firebase storage

--- src/page/Dish.jsx
+++ src/page/Dish.jsx
@@ -1,13 +1,34 @@
-import React from "react"
+import React, {useState} from "react"
 import "./css/Dish.css"
+// import { firebaseApp } from "../firebase/config"
+import firebase from 'firebase';
+const Dish = ({dishInfo}) => {
 
-const Dish = ({dishInfo, pict}) => {
+    const [url , setUrl] = useState("")
+
+    const ref = firebase.storage().ref().child(dishInfo.picture)
+    ref.getDownloadURL().then(url => {
+        let xhr = new XMLHttpRequest()
+        xhr.responseType = 'blob'
+        // xhr.onload =(event) => {
+        //     var blob = xhr.response
+        // }
+        xhr.open('GET', url)
+        xhr.send()
+        console.log(url)
+        setUrl(url)
+    }).catch(err => console.log(err))
+
+    // useEffect(() => {
+    // })
+
+
     return (
         <div className="dish">
             {/* 編集ボタン クリックで編集用のモーダルを開く*/}
             {/* <button type="submit" value={this.state.buttonMode}></button> */}
             {/* 写真 */}
-            <img className="picture" src={pict} alt=""/>
+            <img className="picture" src={url} alt="画像"/>
             <div className="menu-description">
                 {/* 説明 と値段*/}
                 <p className="description">{dishInfo.description}　　　{dishInfo.price}円</p>
