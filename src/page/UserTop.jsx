2180617d5d669e2f617714b9d384f93f7beb5810
hiroki-harada <poketgame.0604@gmail.com>
2020-12-19 09:44:34 +0900

    remove import UserTop.css

--- src/page/UserTop.jsx
+++ src/page/UserTop.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react"
 import Dish from "./Dish"
 import { firebaseApp } from "../firebase/config"
 import { collectionData } from "rxfire/firestore"
-import "./css/UserTop.css"
+// import "./css/UserTop.css"
 import AddMenuModal from "../component/AddMenuModal"
 
 const UserTop = () => {
