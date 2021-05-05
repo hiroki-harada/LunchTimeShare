b40898f6a9bad1955ba8bc331de2523b60a2d515
hiroki-harada <poketgame.0604@gmail.com>
2021-05-05 14:25:22 +0900

    add routing this application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './App.css'
+import {
+    BrowserRouter, Route, Switch
+} from "react-router-dom"
 import UserTop from "./page/UserTop"
 
 const App = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <UserTop></UserTop>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component = {UserTop}/>
+                    </Switch>
+                </BrowserRouter>
             </header>
         </div>
     )
